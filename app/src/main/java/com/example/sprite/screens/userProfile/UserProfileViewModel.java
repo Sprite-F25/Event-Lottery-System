6af97557eb409e164c4bab3548db1dcea268e119
Zenon Poole <zpoole@ualberta.ca>
2025-11-04 22:16:03 -0700

    can now view profile

--- app/src/main/java/com/example/sprite/screens/userProfile/UserProfileViewModel.java
+++ app/src/main/java/com/example/sprite/screens/userProfile/UserProfileViewModel.java
@@ -1,4 +1,16 @@
 package com.example.sprite.screens.userProfile;
 
-public class UserProfileViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.sprite.Controllers.Authentication_Service;
+import com.example.sprite.Models.User;
+import com.example.sprite.R;
+
+public class UserProfileViewModel extends ViewModel {
+    Authentication_Service authService = new Authentication_Service();
+
+
+
 }
