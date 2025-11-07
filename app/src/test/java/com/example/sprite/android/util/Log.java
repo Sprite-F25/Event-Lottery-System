365dd836ab2532d9b3b59eed6d1c31bec487fd36
NaimaU <naimaulusow42@gmail.com>
2025-11-06 22:49:09 -0700

    updating lottery service test

---
+++ app/src/test/java/com/example/sprite/android/util/Log.java
@@ -0,0 +1,12 @@
+//package com.example.sprite.android.util;
+package android.util;
+
+public class Log {
+    public static int e(String tag, String msg) { return 0; }
+    public static int e(String tag, String msg, Throwable tr) { return 0; }
+    public static int i(String tag, String msg) { return 0; }
+    public static int d(String tag, String msg) { return 0; }
+    public static int w(String tag, String msg) { return 0; }
+    public static int w(String tag, String msg, Throwable tr) { return 0; }
+    public static int v(String tag, String msg) { return 0; }
+}
